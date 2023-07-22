4fc814babd90ca386cdf8c7d13b8ad73a2df56b3
Pascal Sochacki <pascal@sochacki.dev>
2023-07-22 09:51:23 +0200

    add channels

--- main.go
+++ main.go
@@ -6,15 +6,36 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Channel struct {
+	Name string
+}
+
 func main() {
+
+	channels := []Channel{
+		{
+			Name: "test",
+		},
+	}
+
 	r := gin.Default()
 	r.LoadHTMLGlob("templates*/*")
 
-	r.Static("/", "./public")
-	r.POST("/clicked", func(ctx *gin.Context) {
+	r.Static("/public", "./public")
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"channels": channels,
+		})
 
-		ctx.HTML(http.StatusOK, "clicked.tmpl", gin.H{
-			"hello": "world",
+	})
+	r.POST("/channels", func(ctx *gin.Context) {
+		channel := &Channel{}
+		if err := ctx.Bind(channel); err != nil {
+			return
+		}
+		channels = append(channels, *channel)
+		ctx.HTML(http.StatusOK, "channels", gin.H{
+			"channels": channels,
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
